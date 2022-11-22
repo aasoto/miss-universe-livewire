553e0f1d4ec520156e17c4b3df8c9c50e3bde7f6
aasoto <andresalfredosotosuarez@gmail.com>
2022-11-22 16:59:25 -0500

    adaptado login nueva plantilla

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,8 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
+    <h1 class="mb-3 sm:mb-5 text-xl sm:text-3xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-rose-900 dark:from-pink-300 dark:to-rose-600">
+        {{__('Login')}}
+    </h1>
+    <div class="border-t border-gray-400 mx-5 sm:mx-10">
         <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -16,16 +15,34 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <input class="text-sm sm:text-base border dark:bg-transparent border-gray-400 rounded-md sm:rounded-lg my-5 px-2 py-1 sm:py-2 w-full focus:text-gray-500 dark:focus:text-white"
+                    placeholder="{{__('Email')}}" id="email" type="email" name="email" required autofocus>
+                <input class="text-sm sm:text-base border dark:bg-transparent border-gray-400 rounded-md sm:rounded-lg mb-5 px-2 py-1 sm:py-2 w-full focus:text-gray-500 dark:focus:text-white"
+                    placeholder="{{ __('Password') }}" id="password" type="password" name="password" required autocomplete="current-password">
             </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <div class="flex flex-col sm:flex-row">
+                <div class="basis-1/2 mb-3 sm:mb-0 flex flex-col justify-center items-center">
+                    <label for="remember_me" class="text-gray-800 dark:text-white text-xs xl:text-base">
+                        <input id="remember_me" name="remember" type="checkbox" class="accent-rose-700 dark:accent-rose-300" checked>
+                        {{ __('Remember me') }}
+                    </label>
+                </div>
+                <div class="basis-1/2 text-center">
+                    <button type="submit" class="rounded-full px-5 py-2 text-white text-xs lg:text-base font-bold bg-gradient-to-r from-pink-600 to-rose-900 dark:from-pink-400 dark:to-rose-700 hover:scale-110 transition">Iniciar sesión</button>
+                </div>
             </div>
-
-            <div class="block mt-4">
+            <div class="flex flex-col iPhoneSE:flex-row justify-center items-center mt-5">
+                <label class="text-gray-800 dark:text-white text-xs xl:text-base font-light" for="">{{__('Forgot your password?')}}</label>
+                @if (Route::has('password.request'))
+                    <a class="ml-2 text-rose-700 dark:text-rose-300 hover:text-rose-900 dark:hover:text-rose-500 hover:underline text-xs xl:text-base font-medium"
+                        href="{{ route('password.request') }}">
+                        {{__('Click here.')}}
+                    </a>
+                @endif
+            </div>
+{{-- ------------------------------------------------------------------------ --}}
+            {{-- <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
@@ -42,7 +59,7 @@
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
-            </div>
+            </div> --}}
         </form>
-    </x-jet-authentication-card>
+    </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,8 +14,91 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+        <div x-data="data()" x-init="dark_mode = JSON.parse(localStorage.getItem('dark_mode'));
+            $watch('dark_mode', value => localStorage.setItem('dark_mode', JSON.stringify(value)))">
+            <div :class="{'dark': dark_mode === true}">
+                <div class="bg-white dark:bg-slate-800">
+                    <div class="relative">
+                        <div class="absolute z-20 ml-10 mt-36 sm:ml-14 sm:mt-24 smd:ml-20 md:ml-7 md:mt-7">
+                            <a href="../index.html">
+                                <div class="block dark:hidden">
+                                    <img class="hidden md:block w-[200px] md:w-auto h-[60px] md:h-[40px] -my-7 md:my-0"
+                                        :src="`${logo_modo_diurno}`" alt="">
+                                    <img class="block md:hidden w-[200px] md:w-auto h-[60px] md:h-[40px] -my-7 md:my-0"
+                                        :src="`${logo_modo_nocturno}`" alt="">
+                                </div>
+                                <div class="hidden dark:block">
+                                    <img class="w-[200px] md:w-auto h-[60px] md:h-[40px] -my-7 md:my-0"
+                                        :src="`${logo_modo_nocturno}`" alt="">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="grid place-items-center h-auto md:h-screen">
+                            <div class="h-screen w-full flex justify-center items-center">
+                                <div class="sm:container w-full md:w-10/12 h-5/6 mt-11 sm:mt-14 md:mt-0">
+                                    <div class="flex flex-col-reverse md:flex-row h-full">
+                                        <div class="basis-0 md:basis-1/2 flex justify-center items-center">
+                                            <div class="z-20 -mt-28 md:mt-0 mb-10 md:mb-0 mr-0 md:-mr-52 lg:-mr-72 xl:-mr-96 flex flex-col justify-center items-center w-4/6 md:w-full lg:w-5/6 h-[290px] sm:h-[350px] bg-white/70 dark:bg-slate-800/70 backdrop-blur-xl shadow-lg rounded-xl">
+                                                {{ $slot }}
+                                            </div>
+                                        </div>
+                                        <div class="basis-0 md:basis-1/2 sm:container flex justify-center items-center">
+                                            <div class="relative w-full md:w-11/12 h-[300px] md:h-full xl:h-5/6 mx-4 md:mx-0">
+                                                <template class="w-full h-full" x-for="image in images">
+                                                    <div class="w-full h-full" x-show="image.show">
+                                                        <img class="-z-10 w-full h-full rounded-xl object-cover object-center blur-xl"
+                                                            :src="`${image.image}`" alt="">
+                                                        <img class="absolute top-0 z-10 w-full h-full rounded-xl object-cover object-center"
+                                                            :src="`${image.image}`" alt="">
+                                                    </div>
+                                                </template>
+                                                <div x-init="change_image()"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
+    <script>
+        function data() {
+            return {
+                logo_modo_diurno: '../storage/app/public/images/dashboard/nav-logo/logo-BPproject-horizontal-fuente.svg',
+                logo_modo_nocturno: '../storage/app/public/images/dashboard/nav-logo/logo-BPproject-horizontal-fuente-blanco.svg',
+                images: [
+                    {
+                        image: '../storage/app/public/images/login/image-1.jpg',
+                        show: true
+                    }, {
+                        image: '../storage/app/public/images/login/image-2.jpg',
+                        show: false
+                    }, {
+                        image: '../storage/app/public/images/login/image-3.jpg',
+                        show: false
+                    }
+                ],
+                change_image() {
+                    i = 0
+                    setInterval(() => {
+                        if (i == 0) {
+                            this.images[this.images.length-1].show = false;
+                            this.images[i].show = true;
+                            i++;
+                        } else {
+                            this.images[i-1].show = false;
+                            this.images[i].show = true;
+                            i++;
+                        }
+                        if (i == this.images.length) {
+                            i = 0;
+                        }
+                    }, 5000);
+                }
+            }
+        }
+    </script>
 </html>
